2137037971104783dd8bbdc9717add4c1ca929a1
EIKE HIRSCH <eike_hirsch@estudante.sesisenai.org.br>
2024-11-06 22:01:36 -0300

    comeca a conectar com o front

---
+++ demo/src/main/java/com/example/demo/configs/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permite acesso de todas as origens (*)
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173/") // Substitua com a URL do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
